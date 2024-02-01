950ccc8abb24c2f2e4ba27856b780def93f7c0a8
Jose Soto <jsoto-2019315@kinal.edu.gt>
2024-02-01 03:01:39 -0600

    Funcionalidad del login completada

---
+++ Asets/Logic/loginLogic.js
@@ -0,0 +1,27 @@
+document.querySelector('#btnLogin').addEventListener('click', iniciarSecion);
+
+function iniciarSecion() {
+    var email = '';
+    var password = '';
+    var acceso = false;
+
+    email = document.querySelector('#userInput').value;
+    password = document.querySelector('#passwordInput').value;
+
+    acceso = validarCredenciales(email, password);
+
+    if (acceso == true) {
+        ingresar();
+    }
+}
+
+function ingresar() {
+    window.location.href = './Asets/Pages/contactos.html';
+}
+
+
+var buttonLogin = document.getElementById('btnLogin');
+
+buttonLogin.addEventListener('click', (e) => {
+    e.preventDefault();
+})
--- Asets/Logic/validacion.js
+++ Asets/Logic/validacion.js
@@ -19,28 +19,29 @@ let users3 = ['Alejandro Max', '7890'];
 let users4 = ['Oscar Morales', '54321'];*/
 
 
+
 function obtenerListaUsuarios(){
     var listaUsuarios = JSON.parse(localStorage.getItem('listaUsuariosLs'));
 
     if (listaUsuarios == null) {
-        listaUsuarios=[
-            ['1', 'José David', 'Soto Puac', 'jsoto-2019315@kinal.edu.gt', '12345', '1']
-            ['2', 'Angel Adolfo', 'Méndez Orozco', 'amendez2019362@kinal.edu.gt', '54321', '1']
+        listaUsuarios = [
+            ['1','José David','Soto Puac','jsoto2019315','12345','2'],
+            ['2','Angel Adolfo','Méndez Orozco','amendez2019362','54321','1']
         ]
     }
-    return listaUsuarios 
+    return listaUsuarios;
 }
 
-function validarCredenciales(pCorreo , pContraseña){
+function validarCredenciales(pCorreo , pPassword){
     var listaUsuarios = obtenerListaUsuarios();
     var acceso = false;
 
     for (var i = 0; i < listaUsuarios.length; i++) {
-        if (pCorreo == listaUsuarios[i][3] && pContraseña == listaUsuarios[i][4]) {
+        if (pCorreo == listaUsuarios[i][3] && pPassword == listaUsuarios[i][4]) {
             acceso = true;
             sessionStorage.setItem('usuarioActivo', listaUsuarios[i][1] + ' ' + listaUsuarios[i][2]);
             sessionStorage.setItem('rolUsuarioActivo', listaUsuarios[i][5]);
-        }        
+        }
     }
     return acceso;
 }
