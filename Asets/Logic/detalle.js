9fe9b71468d5aa64252f74279d8bbe8e4842357e
Jose Soto <jsoto-2019315@kinal.edu.gt>
2024-02-03 20:56:15 -0600

    Efecto de tarjeta desplegable

--- Asets/Logic/detalle.js
+++ Asets/Logic/detalle.js
@@ -1,9 +1,34 @@
+// const flechaDetalle = document.getElementById('aumentar');
+// const detallegeneral1 = document.getElementById('detalleGeneral1');
+// const detalle1 = document.getElementById('detalle1');
+// const infoBasica1 = document.getElementById('infoBasica1');
+// const infoDetallada = document.getElementById('infoDetallada1');
+
 const flechaDetalle = document.getElementById('aumentar');
 const detallegeneral1 = document.getElementById('detalleGeneral1');
 const detalle1 = document.getElementById('detalle1');
-const infoBasica1 = document.getElementById('infoBasica1')
+const infoBasica1 = document.getElementById('infoBasica1');
+const infoDetallada = document.getElementById('infoDetallada1');
 
-flechaDetalle.addEventListener('click' , () => {
-    detalle1.style.display='flex';
-    infoBasica1.style.height='260px';
+let desplegado = false;
+
+flechaDetalle.addEventListener('click', () => {
+  if (desplegado) {
+    // infoDetallada.style.display = 'none';
+    // infoBasica1.style.height = '80px';
+    // flechaDetalle.style.rotate = '0deg';
+
+    infoDetallada.style.display = 'flex';
+    infoBasica1.style.height = '260px';
+    flechaDetalle.style.rotate = '180deg';
+  } else {
+    // infoDetallada.style.display = 'flex';
+    // infoBasica1.style.height = '260px';
+    // flechaDetalle.style.rotate = '180deg';
+
+    infoDetallada.style.display = 'none';
+    infoBasica1.style.height = '80px';
+    flechaDetalle.style.rotate = '0deg';
+  }
+  desplegado = !desplegado;
 });
